bd6c0c44bb192d32250f43d103d412e044a440e4
dhruba371 <dhrubabiswaslogin371@gmail.com>
2024-03-17 02:14:02 +0600

    someting

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+const Banner = () => {
+    return (
+        <div className="bg-image">
+               <div className="section1 text-large"> Discover an exceptional cooking<br/> class tailored for you! </div>
+               <div className="section1 font-small"> Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding<br/> problems to become an exceptionally well world-class Programmer. </div>
+               <div className="section1"><button>Explore Now</button> <button>Our Feedback</button> </div>
+        </div>
+    );
+};
+
+export default Banner;
