02e48fbee4a4b2e9f536d0aea5d8bb9270a249d8
dhruba371 <dhrubabiswaslogin371@gmail.com>
2024-03-17 05:12:29 +0600

    something

--- src/Components/Ourrecipe/Leftrecipe.jsx
+++ src/Components/Ourrecipe/Leftrecipe.jsx
@@ -1,12 +1,19 @@
+import "./Leftrecipe.css";
 
 const Leftrecipe = (Fod) => {
     const recipe = Fod.Fod;
     const {name , info , image , ingredient , calories } = recipe;
+    console.log(image);
     return (
         <div>
-            <h3> Hi I am back again </h3>
-            <h4>name:{}</h4>
-
+            <div className="image"><img src="image" alt="Something worng"></img></div>
+            <div>{name}</div>
+            <div>{info} </div><hr/>
+            <div>Ingredinets:{ingredient.length} </div>
+            <ol>
+                {ingredient.map(ingredien => <li>{ingredien} </li>)}
+            </ol> <hr/>
+            <div className="flex1"><p>HI My </p> <p>Name is</p> </div>
         </div>
     );
 };
